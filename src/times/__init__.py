4249ef2ec346c1f2b3a81872ac193bdd743f00b6
Kyle Horne <me@kyhorne.com>
2022-05-10 17:33:35 -0400

    Add is_weekday and is_trading_day

--- src/times/__init__.py
+++ src/times/__init__.py
@@ -1,4 +1,4 @@
-from .interday import last_trading_day, next_trading_day
+from .interday import is_trading_day, is_weekday, last_trading_day, next_trading_day
 from .vix import (
     last_trading_day_before_vix_expiry,
     last_trading_days_till_expiry,
--- src/times/interday.py
+++ src/times/interday.py
@@ -1,10 +1,18 @@
 import datetime
 
 
+def is_weekday(date: datetime.date) -> bool:
+    return date.weekday() < 5
+
+
+def is_trading_day(date: datetime.date) -> bool:
+    return is_weekday(date)
+
+
 def next_trading_day(date: datetime.date) -> datetime.date:
     date += datetime.timedelta(days=1)
 
-    while date.weekday() > 4:
+    while not is_trading_day(date):
         date += datetime.timedelta(days=1)
 
     return date
@@ -13,7 +21,7 @@ def next_trading_day(date: datetime.date) -> datetime.date:
 def last_trading_day(date: datetime.date) -> datetime.date:
     date -= datetime.timedelta(days=1)
 
-    while date.weekday() > 4:
+    while not is_trading_day(date):
         date -= datetime.timedelta(days=1)
 
     return date
